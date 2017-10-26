d93d5d0a8216f2c9323f472f2044371c00734d38
Stefan Mansson <st.mansson@icloud.com>
2017-10-26 20:57:14 +0300

    Updated web copy
    Added basic design and links to web

--- libs/static/assets/js/app.js
+++ libs/static/assets/js/app.js
@@ -1,16 +1,26 @@
 minicons.icons.forEach(icon => {
-    const iconContainer = document.createElement('div');
-    const iconName = document.createElement('p');
-    const iconElement = document.createElement('i');
-    const testIcons = document.getElementById('icons');
-    iconName.innerText = icon.name;
-    iconContainer.classList.add('col');
-    iconContainer.classList.add('xs-3');
-    iconElement.setAttribute('data-minicon', icon.name);
-    iconContainer.appendChild(iconElement);
-    iconContainer.appendChild(iconName);
-    testIcons.appendChild(iconContainer);
+    const aliases = icon.aliases.length > 0 ? icon.aliases.join(' ') : null;
+    const template = `
+        <div class="col xs-12 m-3">
+            <div class="wrap">
+                <div class="desc">
+                    <p>${icon.name}</p>
+                    <small>Aliases: N/A</small>
+                </div>
+                <div class="icon">
+                    <i data-minicon="${icon.name}"/>
+                </div>
+            </div>
+        </div>`;
+
+    const templateElement = $(template);
+    $('#icons').append(templateElement);
+
+    if (aliases !== null) {
+        templateElement.find('.desc small').html(`Aliases: <strong>${aliases}</strong>`)
+    }
 });
+
 minicons.setOptions({
     "observe" : true,
     "config" : {
@@ -20,4 +30,5 @@ minicons.setOptions({
         }
     }
 });
+
 minicons.swap();
